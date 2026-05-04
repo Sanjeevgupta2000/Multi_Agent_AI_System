0664e58ae1880aae3214cdca1404f505223b4f03
Sanjeev Gupta <159869847+Sanjeevgupta2000@users.noreply.github.com>
2026-05-04 12:29:27 +0530

    pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
-from agents import build_reader_agent,build_search_agent,critic_chain,writer_chain
+from agents import build_reader_agent , build_search_agent , writer_chain , critic_chain
+
 def run_research_pipeline(topic : str) -> dict:
+
     state = {}
 
     #search agent working 
@@ -15,36 +17,34 @@ def run_research_pipeline(topic : str) -> dict:
 
     print("\n search result ",state['search_results'])
 
-    # step 2 reader agent
+    #step 2 - reader agent 
     print("\n"+" ="*50)
-    print("step 2 - reader agent is scaraping top resources ...")
+    print("step 2 - Reader agent is scraping top resources ...")
     print("="*50)
-
 
     reader_agent = build_reader_agent()
     reader_result = reader_agent.invoke({
-"messages": [("user",
+        "messages": [("user",
             f"Based on the following search results about '{topic}', "
             f"pick the most relevant URL and scrape it for deeper content.\n\n"
             f"Search Results:\n{state['search_results'][:800]}"
         )]
-
     })
 
     state['scraped_content'] = reader_result['messages'][-1].content
+
     print("\nscraped content: \n", state['scraped_content'])
 
-    # Step 3 Writer Chain
-    print("\n"+" ="*50)
-    print("step 3 - writer the drafting the report ...")
-    print("="*50)
+    #step 3 - writer chain 
 
+    print("\n"+" ="*50)
+    print("step 3 - Writer is drafting the report ...")
+    print("="*50)
 
     research_combined = (
         f"SEARCH RESULTS : \n {state['search_results']} \n\n"
         f"DETAILED SCRAPED CONTENT : \n {state['scraped_content']}"
     )
-
 
     state["report"] = writer_chain.invoke({
         "topic" : topic,
@@ -66,6 +66,7 @@ def run_research_pipeline(topic : str) -> dict:
     print("\n critic report \n", state['feedback'])
 
     return state
+
 
 
 if __name__ == "__main__":
