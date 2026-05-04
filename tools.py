9ed8de95d7ee9f2eb370a984d307b9fa5d9137e8
Sanjeev Gupta <159869847+Sanjeevgupta2000@users.noreply.github.com>
2026-05-04 12:30:22 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -1,26 +1,28 @@
-from langchain_core.tools import tool
+from langchain.tools import tool 
 import requests
 from bs4 import BeautifulSoup
-from rich import print
 from tavily import TavilyClient
+import os 
+from dotenv import load_dotenv
+from rich import print
+load_dotenv()
 
-
-tavily=TavilyClient(api_key="Enter your api keys")
+tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 @tool
-def web_search(query :str) -> str:
-    """ Search on web for recent and reliable information on a topic.Retrun Title,URLs and snippets"""
-    result=tavily.search(query=query,max_results=5)
+def web_search(query : str) -> str:
+    """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
+    results = tavily.search(query=query,max_results=5)
+
     out = []
 
-    for r in result['results']:
+    for r in results['results']:
         out.append(
             f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
         )
     
     return "\n----\n".join(out)
 
-    # return result
 @tool
 def scrape_url(url: str) -> str:
     """Scrape and return clean text content from a given URL for deeper reading."""
@@ -32,4 +34,3 @@ def scrape_url(url: str) -> str:
         return soup.get_text(separator=" ", strip=True)[:3000]
     except Exception as e:
         return f"Could not scrape URL: {str(e)}"
-    
